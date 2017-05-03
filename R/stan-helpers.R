206711c555d9bbd89227c8736929bcd349603a4d
Alex Forrence <aforren1@jhu.edu>
2017-05-03 15:25:14 -0400

    Quick commit

--- R/stan-helpers.R
+++ R/stan-helpers.R
@@ -406,7 +406,7 @@ stan_families <- function(family, bterms) {
   } else if (any(families %in% "hurdle_lognormal")) {
     out$fun <- "  #include 'fun_hurdle_lognormal.stan' \n"
   } else if (any(families %in% "exgaussian")) {
-    out$fun <- "  #include 'fun_exgaussian.stan' \n"
+    #out$fun <- "  #include 'fun_exgaussian.stan' \n"
   } else if (any(families %in% "inverse.gaussian")) {
     out$fun <- "  #include 'fun_inv_gaussian.stan' \n"
     out$tdataD <- "  #include 'tdataD_inv_gaussian.stan' \n"
